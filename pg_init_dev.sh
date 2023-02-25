5fbea1ff27d038eaccd1739721f16f0f9cf7d677
Kirk Wolak <kirkw@users.noreply.github.com>
2023-02-25 00:05:10 -0500

    Update pg_init_dev.sh

--- pg_init_dev.sh
+++ pg_init_dev.sh
@@ -3,3 +3,5 @@ echo $(date +'%H:%M:%S') Starting Script
 echo complete
 echo $(date +'%H:%M:%S') Script Complete
 export PS1='\[\033[01;31m\]\D{%H:%M:%S} \[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+echo '"\e[19~": history-search-backward' > ~/.inputrc
+# I want a way
