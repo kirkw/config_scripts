fdccacc3afb5a7931e4e2f987513960880da7b5d
Kirk Wolak <kirkw@users.noreply.github.com>
2023-02-24 22:19:04 -0500

    Update pg_init_dev.sh

--- pg_init_dev.sh
+++ pg_init_dev.sh
@@ -2,3 +2,4 @@ echo THIS IS A TEST SCRIPT \(parens used\)
 echo $(date +'%H:%M:%S') Starting Script
 echo complete
 echo $(date +'%H:%M:%S') Script Complete
+PS1='\[\033[01;31m\]\D{%H:%M:%S} \[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
