4525a68d50dd7f5c0cf966107135d2c58d5ff0a4
Kirk Wolak <kirkw@users.noreply.github.com>
2023-02-25 12:24:38 -0500

    Update pg_init_dev.sh

--- pg_init_dev.sh
+++ pg_init_dev.sh
@@ -3,6 +3,8 @@ echo $(date +'%H:%M:%S') Starting Script
 echo complete
 echo $(date +'%H:%M:%S') Script Complete
 export PS1='\[\033[01;31m\]\D{%H:%M:%S} \[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+# I need a way to change my bashrc and my inputrc, and my prompt
+echo "PS1='\[\033[01;31m\]\D{%H:%M:%S} \[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
 # F8 is an addiction (type 2-3 characters, hit F8 to search backwards for that command.  Imagine !vs + AUTO COMPLETE without execute
 echo '"\e[19~": history-search-backward' >> ~/.inputrc
-# I need a way to change my bashrc and my inputrc
+echo '"\C-x\C-k": kill-whole-line' >>  ~/.inputrc
